2ae017c4f5a42bab15a6d3228a832f427206e10b
Spazzlefrazzle <juuncaerlum@gmail.com>
2018-10-17 10:49:54 -0400

    Setting up sample unit tests (took a while to figure out JUnit 5 with Gradle)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,4 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
     }
+
+    public int methodForTesting() {
+        return 1;
+    }
 }
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void methodForTesting() {
+        Main fixture = new Main();
+        assertEquals(1, fixture.methodForTesting());
+    }
+}
